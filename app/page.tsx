f49ef352d7e4c35f8f3380180a56ada8f3209e7a
Yahya Ahmed <yahyadahirahmed@outlook.com>
2024-12-25 16:37:57 +0000

    login feature complete

--- app/page.tsx
+++ app/page.tsx
@@ -80,21 +80,7 @@ export default function Home() {
           />
           Examples
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
