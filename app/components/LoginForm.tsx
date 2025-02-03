90abf9fe40f726bc4a1b919ebf9af22c7bba6d8d
Yahya Ahmed <yahyadahirahmed@outlook.com>
2025-02-03 15:37:50 +0000

    deployment changes

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { signIn, useSession, getSession } from "next-auth/react";
+import { signIn, getSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
