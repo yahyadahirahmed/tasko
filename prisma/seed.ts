2400ba4086aca5b2aeaf8137fef09a0c7be4ff45
Yahya Ahmed <yahyadahirahmed@outlook.com>
2025-02-18 18:41:36 +0000

    def

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,25 @@
-import { prisma } from '../app/lib/prisma';
-import bcrypt from 'bcryptjs';
-import { TaskState, TaskPriority } from '../app/types';
+// import { prisma } from '../app/lib/prisma';
+// import bcrypt from 'bcryptjs';
+// import { TaskState, TaskPriority } from '../app/types';
 
 
 
-async function main() {
+// async function main() {
   // await prisma.task.deleteMany();
   // await prisma.user.deleteMany();
 
   // first users in db
-  const admin = await prisma.user.findFirst({
-    where: { userType: 'ADMIN' }
-  });
+  // const admin = await prisma.user.findFirst({
+  //   where: { userType: 'ADMIN' }
+  // });
   
-  const member = await prisma.user.findFirst({
-    where: { userType: 'TEAM_MEMBER' }
-  });
+  // const member = await prisma.user.findFirst({
+  //   where: { userType: 'TEAM_MEMBER' }
+  // });
   
-  if (!admin || !member) {
-    throw new Error('Required users not found. Please ensure admin and team member exist.');
-  }
+  // if (!admin || !member) {
+  //   throw new Error('Required users not found. Please ensure admin and team member exist.');
+  // }
 
   // console.log('Seeding database...');
 
@@ -76,27 +76,27 @@ async function main() {
   //   },
   // });
 
-  const today = new Date();
-  today.setHours(23, 59, 59, 999);
+//   const today = new Date();
+//   today.setHours(23, 59, 59, 999);
   
-  // Create tasks
-  await prisma.task.createMany({
-    data: [
-      { text: 'High Priority Task', state: TaskState.ToDo, priority: TaskPriority.high, createdById: admin.id, assignedToId: member.id,deadline: new Date() },
-      { text: 'Medium Priority Task', state: TaskState.ToDo, priority: TaskPriority.medium, createdById: admin.id, assignedToId: member.id, deadline: new Date() },
-      { text: 'Low Priority Task', state: TaskState.ToDo, priority: TaskPriority.low, createdById: admin.id, assignedToId: member.id, deadline: new Date() },
-    ],
-  });
-// console.log({ admin, member, manager });
-}
+//   // Create tasks
+//   await prisma.task.createMany({
+//     data: [
+//       { text: 'High Priority Task', state: TaskState.ToDo, priority: TaskPriority.high, createdById: admin.id, assignedToId: member.id,deadline: new Date() },
+//       { text: 'Medium Priority Task', state: TaskState.ToDo, priority: TaskPriority.medium, createdById: admin.id, assignedToId: member.id, deadline: new Date() },
+//       { text: 'Low Priority Task', state: TaskState.ToDo, priority: TaskPriority.low, createdById: admin.id, assignedToId: member.id, deadline: new Date() },
+//     ],
+//   });
+// // console.log({ admin, member, manager });
+// }
 
 
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
